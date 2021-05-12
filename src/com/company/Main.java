82acdb06584c40e3f4f0451be9df37c733d8091c
Manmehak <mehakpartap@gmail.com>
2021-05-12 23:09:31 +0530

    Accessed static content outside main method by adding name variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
 public class Main {
+    private static String _name = "Manmehak"; // used _ as my own convention to show it is a private instance variable
+    // static means to access the static content out side the static main method
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Program Started! ");
+        System.out.println(_name);
     }
 }
